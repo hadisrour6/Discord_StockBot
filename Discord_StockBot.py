88c87e2951024f689cf5baae0fb4afcadbdc6a3e
Hadi Srour <70966830+hadisrour6@users.noreply.github.com>
2022-08-15 18:27:17 -0400

    Update Discord_StockBot.py

--- Discord_StockBot.py
+++ Discord_StockBot.py
@@ -24,7 +24,10 @@ async def about(ctx):
                    "-> .about  will bring up my commands\n"
                    "-> .price or .$ followed by any valid ticker will bring up its price for example: .price AAPL or .$ AAPL\n"
                    "-> .trade or .Trade will begin the 'CryptoTrading' script that will begin calculating RSI and buying/selling ETH "
-                   "\t\tbased on RSI. When an order is made an update is sent to users phone through text "
+                   "\t\tbased on RSI. When an order is made an update is sent to users phone through text\n"
+                   "-> .sector or .sec followed by any valid ticker will return the sector said company is in\n"
+                   "-> .dividends or .div followed by any valid ticker will return the amount of dividends the company gives out to its share holders\n"
+                   "-> .shares or .Shares followed by any valid ticker will return the number of outstanding shares a company has  "
                   )
 
 @client.command(aliases=['$'])
@@ -35,6 +38,29 @@ async def price(ctx,*, ticker):
     time = str(datetime.datetime.now().strftime(' %Y/%m/%d - %I:%M:%S %p EST'))
 
     await ctx.send("-> $" + str(round(get_live_price(ticker),3)) + " <- " + company_name + " ($" + ticker.upper() + ")" + " as of " + time)
+
+
+@client.command(aliases=['sec'])
+async def sector(ctx,*, ticker):
+    company = Ticker(ticker)
+    company_sector = company.info['sector']
+
+    await ctx.send(company_sector)
+
+@client.command(aliases=['Shares'])
+async def shares(ctx,*, ticker):
+    company = Ticker(ticker)
+    company_shares = company.info['sharesOutstanding']
+
+    await ctx.send(company_shares)
+
+@client.command(aliases=['div'])
+async def dividends(ctx,*, ticker):
+    company = Ticker(ticker)
+    company_dividends = company.info['dividends']
+
+    await ctx.send(company_dividends)
+
 
 @client.command(aliases=['Trade'])
 async def trade(ctx):
